c4f6751a627ff9fe77e23e7178ff2fa8eb5ae42d
Anirudh <anirudhmitra210@gmail.com>
2023-03-07 22:44:28 +0530

    gin init

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/Anirudh4583/go-gin-template/pkg/setting"
+	"github.com/gin-gonic/gin"
 )
 
 func init() {
@@ -9,5 +14,26 @@ func init() {
 }
 
 func main() {
-	println("Hello Hoegarden API!")
+	println("Hello GO API!")
+
+	gin.SetMode(setting.Config.ServerSetting.RunMode)
+
+	port := fmt.Sprintf(":%d", setting.Config.ServerSetting.HttpPort)
+	router := gin.Default()
+	router.GET("/health", func(ctx *gin.Context) {
+		ctx.String(200, "health check passed!")
+	})
+	readTimout := setting.Config.ServerSetting.ReadTimeout
+	writeTimeout := setting.Config.ServerSetting.WriteTimeout
+
+	s := &http.Server{
+		Addr:           port,
+		Handler:        router,
+		ReadTimeout:    readTimout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Printf("[info] start http server listening %s", port)
+	s.ListenAndServe()
 }
