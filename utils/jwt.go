60e191d840c242c0c7ca46acd26b929313de3972
Anirudh <anirudhmitra210@gmail.com>
2023-03-11 19:17:18 +0530

    jwt + util setup

---
+++ utils/jwt.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret []byte
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(username string) (string, error) {
+	nowTime := time.Now()
+	expTime := time.Now().Add(24 * time.Hour)
+	claims := Claims{
+		username, // TODO: use some kind of encoding/hashing before storing in token
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expTime),
+			IssuedAt:  jwt.NewNumericDate(nowTime),
+			Issuer:    "go-gin-template", // TODO: change this to your app name
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedString, err := token.SignedString(jwtSecret)
+
+	return signedString, err
+}
+
+func ParseToken(tokenString string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+	return nil, err
+}
---
+++ utils/util.go
@@ -0,0 +1,7 @@
+package util
+
+import "github.com/Anirudh4583/go-gin-template/pkg/setting"
+
+func Setup() {
+	jwtSecret = []byte(setting.Config.AppSetting.JwtSecret)
+}
