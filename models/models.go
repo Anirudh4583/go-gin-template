c0ea1145c3282c2227d18b49eb08289809cad9c8
Anirudh <anirudhmitra210@gmail.com>
2023-03-12 19:49:04 +0530

    chore: vendor

--- models/models.go
+++ models/models.go
@@ -6,25 +6,24 @@ import (
 
 	"github.com/Anirudh4583/go-gin-template/pkg/setting"
 	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
 )
 
-func Setup(){
+func Setup() {
 
-    dbURI := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
-        setting.Config.DatabaseSetting.Host,
-        setting.Config.DatabaseSetting.Port, 
-        setting.Config.DatabaseSetting.Name,
-        setting.Config.DatabaseSetting.User, 
-        setting.Config.DatabaseSetting.Password)
+	dbURI := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		setting.Config.DatabaseSetting.Host,
+		setting.Config.DatabaseSetting.Port,
+		setting.Config.DatabaseSetting.Name,
+		setting.Config.DatabaseSetting.User,
+		setting.Config.DatabaseSetting.Password)
 
-    // Connect to the database
-    db, err := gorm.Open("postgres", dbURI)
-    if err != nil {
-        log.Fatalf("Error connecting to the database: %v", err)
-    }
+	// Connect to the database
+	db, err := gorm.Open("postgres", dbURI)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
 
-    defer db.Close()
+	defer db.Close()
 
-    fmt.Println("Connected to the database!")   
+	fmt.Println("Connected to the database!")
 }
