d223ff16eac7a2cda21b570ff4861740cb81bfe3
Anirudh <anirudhmitra210@gmail.com>
2023-03-12 23:00:17 +0530

    chore: vendor

--- utils/hash.go
+++ utils/hash.go
@@ -1,5 +1,7 @@
 package util
 
+import "golang.org/x/crypto/bcrypt"
+
 func Hash(password string) string {
 	hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(hash)
